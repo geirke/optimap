f0ef2404fb4a88d6d4448da8c875b47406c3c3ff
Mica Cardillo <mica.cardillo@gmail.com>
2014-08-22 11:33:21 -0400

    remove developer's Google Analytics

--- index.php
+++ index.php
@@ -368,14 +368,6 @@ if (!$hidePoll2) {
 <iframe name='garminIFrame' style='width: 580px; height: 400px'></iframe>
 </div>
 
-<script type="text/javascript">
-var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-</script>
-<script type="text/javascript">
-var pageTracker = _gat._getTracker("UA-3472506-1");
-pageTracker._initData();
-pageTracker._trackPageview();
-</script>
+
 </body>
 </html>
