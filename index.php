7c5a7a4d0c7a6758d28ed0df32c51ad6ca09a0a8
Mica Cardillo <mica.cardillo@gmail.com>
2014-08-22 12:59:49 -0400

    removed developer's Twitter widget

--- index.php
+++ index.php
@@ -189,7 +189,6 @@ jQuery(function() {
     })();
 /* ]]> */</script>
 
-<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 
 </head>
 
